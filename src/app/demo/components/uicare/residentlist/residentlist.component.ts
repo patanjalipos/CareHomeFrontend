db4cc21e2d863021bf8df92a77f012f216031e0c
Abhishek <abhishek.agrawal@bharuwasolution.com>
2022-11-09 15:28:12 +0530

    new changes

--- src/app/demo/components/uicare/residentlist/residentlist.component.ts
+++ src/app/demo/components/uicare/residentlist/residentlist.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Table } from 'primeng/table';
+import { DataView } from 'primeng/dataview';
 import { ResidentList } from 'src/app/demo/classes/ResidentList';
 import { CareService } from 'src/app/demo/service/CareServices';
 
@@ -9,19 +10,29 @@ import { CareService } from 'src/app/demo/service/CareServices';
   styleUrls: ['./residentlist.component.scss']
 })
 export class ResidentlistComponent implements OnInit {
-  residents!: ResidentList[];
-  @ViewChild('filter') filter!: ElementRef;
+  @ViewChild('filter') filter: ElementRef;
+  residents: ResidentList[];
+  viewMode: boolean = true;
+  residentId: string = "";
+  activeClass: string = "";
   constructor(private careService: CareService) { }
 
   ngOnInit(): void {
     this.careService.getResidentList().then(data => this.residents = data);
+    this.viewMode = true;
   }
+  
   onGlobalFilter(table: Table, event: Event) {
     table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
   }
   clear(table: Table) {
     table.clear();
     this.filter.nativeElement.value = '';
-}
-
+  }
+  onFilter(dv: DataView, event: Event) {
+    dv.filter((event.target as HTMLInputElement).value);
+  }
+  residentClick(id) {
+    this.residentId = id;
+  }
 }
--- src/app/demo/components/uicare/residentlist/residentlist.module.ts
+++ src/app/demo/components/uicare/residentlist/residentlist.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { ResidentlistComponent } from './residentlist.component';
 import { ResidentlistRoutingModule } from './residentlist-routing.module';
 import { TableModule } from 'primeng/table';
-
-
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from 'primeng/button';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { DataViewModule } from 'primeng/dataview';
 @NgModule({
 declarations: [ResidentlistComponent],
 imports: [
     CommonModule,
+    FormsModule,
     ResidentlistRoutingModule,
-    TableModule
+    TableModule,
+    InputTextModule,
+    ButtonModule,
+    ToggleButtonModule,
+    DataViewModule
   ]})
 export class ResidentlistModule { }
