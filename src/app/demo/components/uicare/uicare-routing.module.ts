bfbd76031e605cb4c75b6178a2c25adbb1ef4f59
gauravbana72 <gauravbana72@gmail.com>
2023-02-15 16:07:00 +0530

    new changes

--- src/app/demo/components/uicare/uicare-routing.module.ts
+++ src/app/demo/components/uicare/uicare-routing.module.ts
@@ -7,7 +7,8 @@ import { RouterModule} from '@angular/router';
     { path: 'residentlist', data: { breadcrumb: 'Resident List'}, loadChildren: () => import('./residentlist/residentlist.module').then(m => m.ResidentlistModule) },
     { path: 'residentprofile', data: { breadcrumb: 'Resident Profile'}, loadChildren: () => import('./resident-profile/resident-profile.module').then(m => m.ResidentProfileModule) },
     { path: 'fallriskassessmentreport', data: { breadcrumb: 'Fall Risk Assessment Report'}, loadChildren: () => import('./fall-risk-assessment-report/fall-risk-assessment-report.module').then(m => m.FallRiskAssessmentReportModule) },
-    { path: '**', redirectTo: '/notfound' }
+    { path: '**', redirectTo: '/notfound' },
+    { path: 'user-profile', data: { breadcrumb: 'User Profile'}, loadChildren: () => import('./user-profile/user-profile.module').then(m => m.UserProfileModule) }
   ])],
   exports: [RouterModule]
 })
