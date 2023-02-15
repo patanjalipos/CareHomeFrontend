1a357f489ec8c2e5f1a6af856bdebdadef6a667a
Mrinal Roy <mrinal.roy@bharuwasolution.com>
2023-02-15 16:20:51 +0530

    New Changes

--- src/app/ui/components/uicare/uicare-routing.module.ts
+++ src/app/ui/components/uicare/uicare-routing.module.ts
@@ -7,6 +7,7 @@ import { RouterModule} from '@angular/router';
     { path: 'residentlist', data: { breadcrumb: 'Resident List'}, loadChildren: () => import('./residentlist/residentlist.module').then(m => m.ResidentlistModule) },
     { path: 'residentprofile', data: { breadcrumb: 'Resident Profile'}, loadChildren: () => import('./resident-profile/resident-profile.module').then(m => m.ResidentProfileModule) },
     { path: 'fallriskassessmentreport', data: { breadcrumb: 'Fall Risk Assessment Report'}, loadChildren: () => import('./fall-risk-assessment-report/fall-risk-assessment-report.module').then(m => m.FallRiskAssessmentReportModule) },
+    { path: 'user-profile', data: { breadcrumb: 'User Profile'}, loadChildren: () => import('./user-profile/user-profile.module').then(m => m.UserProfileModule) },
     { path: '**', redirectTo: '/notfound' }
   ])],
   exports: [RouterModule]
