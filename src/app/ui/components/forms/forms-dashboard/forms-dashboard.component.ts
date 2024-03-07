391139e046de39ccf221ba8427e0fed907d152a1
Ishaan Kr. <ishaan.kumar@bharuwasolutions.com>
2024-03-07 16:55:14 +0530

    Create and Search Tab added in Form Module Dashboard

--- src/app/ui/components/forms/forms-dashboard/forms-dashboard.component.ts
+++ src/app/ui/components/forms/forms-dashboard/forms-dashboard.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-forms-dashboard',
-  templateUrl: './forms-dashboard.component.html',
-  styleUrls: ['./forms-dashboard.component.scss']
+    selector: 'app-forms-dashboard',
+    templateUrl: './forms-dashboard.component.html',
+    styleUrls: ['./forms-dashboard.component.scss'],
 })
 export class FormsDashboardComponent implements OnInit {
+    //Tab 1 Data
+    stlststatus: any[] = [];
+    status: number;
+    //Tab 2 Data
+    date1: Date | undefined;
+    date2: Date | undefined;
 
-  constructor() { }
+    constructor() {
+        this.stlststatus = [
+            { name: 'Active', code: 1 },
+            { name: 'Inactive', code: 0 },
+        ];
+    }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit() {}
 }
--- src/app/ui/components/forms/forms-dashboard/forms-dashboard.module.ts
+++ src/app/ui/components/forms/forms-dashboard/forms-dashboard.module.ts
@@ -4,19 +4,33 @@ import { CommonModule } from '@angular/common';
 import { FormsDashboardRoutingModule } from './forms-dashboard-routing.module';
 import { FormsDashboardComponent } from './forms-dashboard.component';
 
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+
+
 
 @NgModule({
   declarations: [
-    
     FormsDashboardComponent
   ],
   imports: [
     CommonModule,
-    FormsDashboardRoutingModule
+    FormsDashboardRoutingModule,
+    ToolbarModule,
+    ButtonModule,
+    InputTextModule,
+    CalendarModule,
+    FormsModule,
+    DropdownModule
   ],
 
   exports:[
     FormsDashboardComponent,
   ]
 })
+
 export class FormsDashboardModule { }
